0a8bc4d262cbda9997b75c0896cd95cbe8eb9332
rafaelramos64 <rafaelmaggot64@gmail.com>
2022-07-29 12:02:54 -0300

    Inicializar projeto confugurado para desenvolvimento.

---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+   
+    <v-footer :absolute="!fixed" app>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+  data () {
+    return {
+      fixed: false,
+    }
+  },
+}
+</script>
